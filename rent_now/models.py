5e4940dda1be53f8cb09f688302b40b8c6d1d3d5
Rajat <85779110+rajatDeveloper@users.noreply.github.com>
2024-06-23 15:26:47 +0530

    Update models.py

--- rent_now/models.py
+++ rent_now/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
-
+# hi i am dev 
 class Category (models.Model):
+    
     name = models.CharField(max_length=100)
     description = models.TextField()
     category_image = models.ImageField(upload_to='category_images/')
